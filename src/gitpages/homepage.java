11627b0a75070f33eebefa41367cfb60e435a23a
Tejaswini <tesjaswinimodgekar@gmail.com>
2024-03-15 11:14:46 +0530

    first commit

---
+++ src/gitpages/homepage.java
@@ -0,0 +1,19 @@
+package gitpages;
+
+public class homepage {
+	public void getTiltleofpage()
+	{
+		System.out.println("page title");
+	}
+	public void searchProducts()
+	{
+		System.out.println("searching the products");
+		
+	}
+	public void addage()
+	{
+		System .out.println("age");
+	
+	}
+
+}
